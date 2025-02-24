332434547819ae4a1dfbe6f061345a45bd639d72
Fábio Balbé <fabiobalbe@mac.local>
2025-02-23 23:59:33 -0300

    Criei 3 botões com para os principais subsistemas; Linkei os mesmos na nav-bar

--- barra-nav.php
+++ barra-nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php"><?php echo $nome_sistema ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,11 +10,15 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Primeiro</a>
+          <a class="nav-link" href="#">Pacientes</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Segundo</a>
+          <a class="nav-link" href="#">Nova Consulta</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Histórico de Consultas</a>
+        </li>
+
       </ul>
     </div>
   </div>
--- index.php
+++ index.php
@@ -13,7 +13,20 @@ include_once 'topo.php';
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
   <div class="container col-11 bg-light text-dark mx-auto p-3 mt-4 rounded-2 shadow altura-85">
-    <h1>Hello, world!</h1>
+    <div class="container text-center" style="height: 100%;">
+      <div class="row align-items-center" style="height: 100%;">
+        <div class="col d-flex align-items-center justify-content-center p-3 rounded-2 bg-warning text-white h-25 shadow">
+          <h1>Pacientes</h1>
+        </div>
+        <div class="col d-flex align-items-center justify-content-center p-3 ms-3 me-3 rounded-2 bg-success text-white h-25 shadow">
+          <h1>Nova Consulta</h1>
+        </div>
+        <div class="col d-flex align-items-center justify-content-center p-3 bg-dark rounded-2 text-white h-25 shadow">
+          <h1>Histórico de Atendimento</h1>
+        </div>
+      </div>
+    </div>
+
   </div>
 </body>
 
