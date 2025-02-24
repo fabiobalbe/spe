af1ed0ec37b4c8cdb5e0dd2af723dbdf19bcc7af
Fábio Balbé <fabiobalbe@mac.local>
2025-02-23 21:22:07 -0300

    Retirei a propriedade INTEGRITY da importação do css e js

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 $nome_pagina = "Inicio";
 require 'config.php';
 include_once 'topo.php';
-include_once 'barra-nav.php';
 ?>
 
 </head>
 
 <body class="bg-success">
+  <?php include_once 'barra-nav.php'; ?>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-  <div class="container col-11 bg-light text-dark mx-auto p-3 mt-4 rounded-2 shadow-lg altura-85">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <div class="container col-11 bg-light text-dark mx-auto p-3 mt-4 rounded-2 shadow altura-85">
     <h1>Hello, world!</h1>
   </div>
 </body>
--- topo.php
+++ topo.php
@@ -12,5 +12,5 @@ function titulo($ns, $np)
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo titulo($nome_sistema, $nome_pagina) ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-qwtkzyjppejisv5waru9oferpok6yctnymdr5pnlyt2brjxh0jmhjy6hw+alewih" crossorigin="anonymous">
-  <link rel="stylesheet "href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
