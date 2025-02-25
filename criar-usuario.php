fb2d897fc945d5e990ec1a130761eac943326e9f
Fábio Balbé <fabiobalbe@mac.local>
2025-02-24 23:05:26 -0300

    SERVER SIDE VALIDATION on criar-usuário.php

--- criar-usuario.php
+++ criar-usuario.php
@@ -1,14 +1,27 @@
 <?php
-if (empty($_POST["nome"])) {
+if (empty($_POST["nome"])) {                                         // VERIFICA SE O NOME E EMAIL ESTÃO PREENCHIDOS.
   die("Faltou o nome!");
 }
 if (empty($_POST["email"])) {
   die("Faltou o email!");
 }
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {           // VERIFICA SE O EMAIL É VÁLIDO.
+  die("O endereço de E-Mail está incorreto!");
+}
 if (empty($_POST["senha"])) {
   die("Faltou a senha!");
 }
-if (empty($_POST["confirmar-senha"])) {
-  die("Faltou confirmar a senha!");
+if (strlen($_POST["senha"]) < 8) {                                   // VERIFICA SE A SENHA TEM AO MENOS 8 CARACTERES.
+  die("Senha deve ter ao menos 8 caracteres!");
 }
+if (!preg_match("/[a-z]/i", $_POST["senha"])) {                      // VERIFICA SE A SENHA TEM AO MENOS UMA LETRA.
+  die("Senha deve ter ao menos uma letra");
+}
+if (!preg_match("/[0-9]/i", $_POST["senha"])) {                      // VERIFICA SE A SENHA TEM AO MENOS UM NÚMERO.
+  die("Senha deve ter ao menos um número!");
+}
+if ($_POST["senha"] !== $_POST["confirmar-senha"]) {                 // VERIFICA SE A SENHA E O CONFIRMAR-SENHA SÃO 
+  die("As senhas não são inguais!");                                 // IDENTIDOS.
+}
+
 print_r($_POST);
