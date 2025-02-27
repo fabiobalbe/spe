1edab9c256ba102dea5e543cc87d0fde85ba41ea
Fábio Balbé <fabiobalbe@mac.local>
2025-02-26 22:17:54 -0300

    Autenticação de login por user_id

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+require 'auth/verifica.php';
 $nome_pagina = "Inicio";
 require 'config.php';
 include_once 'topo.php';
@@ -14,6 +16,7 @@ include_once 'topo.php';
   <div class="container col-11 bg-light text-dark mx-auto p-3 mt-4 rounded-2 shadow altura-85">
     <div class="container text-center" style="height: 100%;">
       <div class="row align-items-center" style="height: 100%;">
+        <h1>Bem vindo <?= $_SESSION["user_id"] ?></h1>
         <div class="col d-flex align-items-center justify-content-center p-3 rounded-2 bg-warning text-white h-25 shadow">
           <h1>Pacientes</h1>
         </div>
