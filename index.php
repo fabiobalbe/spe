aa8d0d025b27670be9ed3e03703c4980c3592b89
Fábio Balbé <fabiobalbe@mac.local>
2025-02-28 14:07:08 -0300

    Pequenos ajustes em index.php

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Define uma constante que libera acesso aos arquivos componentes incluidos abaixo.
+// Define uma constante que libera acesso aos arquivos componentes incluídos abaixo.
 define('ACCESS_ALLOWED', true);
-
 
 // Define o nome da página.
 $nome_pagina = "Inicio";
 
-
 // Autenticação de usuário com controle de erro do REQUIRE.
-$file = __DIR__ . '/auth/verifica.php';
-if (file_exists($file)) {
-  require $file;
+$arquivo_verifica = __DIR__ . '/auth/verifica.php';
+if (file_exists($arquivo_verifica)) {
+  require $arquivo_verifica;
 } else {
   die("Erro crítico: arquivo de autenticação de usuário não encontrado.");
-  exit;
 }
 
-
 // Configurações globais com controle de erro do REQUIRE.
-$file = __DIR__ . '/config.php';
-if (file_exists($file)) {
-  require $file;
+$arquivo_config = __DIR__ . '/config.php';
+if (file_exists($arquivo_config)) {
+  require $arquivo_config;
 } else {
   die("Erro crítico: arquivo de configuração não encontrado.");
-  exit;
 }
 
 // Chama o componente TOPO.PHP.
 require_once __DIR__ . '/componentes/topo.php';
+
 ?>
 
 </head>
@@ -42,7 +41,9 @@ require_once __DIR__ . '/componentes/topo.php';
     <?php require_once __DIR__ . '/paginas/painel.php'; ?>
 
   </div>
+
 </body>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
