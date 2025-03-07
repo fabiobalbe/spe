b200542b9818a138d28bb46395e535ea49942b30
Fábio Balbé <fabiobalbe@mac.local>
2025-03-07 15:10:39 -0300

    API verifica se existe cpf cadastrado

---
+++ api/api-existe-cpf.php
@@ -0,0 +1,35 @@
+<?php
+require_once dirname(__DIR__) . "/auth/database.php";
+
+if (isset($_GET["cpf"])) {
+  $cpf = $_GET["cpf"];
+
+  $sql = "SELECT * FROM pacientes WHERE cpf = ?";
+
+  $stmt = $mysqli->stmt_init();
+
+  if (!$stmt->prepare($sql)) {
+    die("Erro SQL: " . $mysqli->error);
+  }
+
+  $stmt->bind_param(
+    "s",
+    $cpf
+  );
+
+  if ($stmt->execute()) {
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+      header("Content-Type: application/json");
+      echo json_encode(["isValid" => true]);
+    } else {
+      header("Content-Type: application/json");
+      echo json_encode(["isValid" => false]);
+    }
+    exit;
+  } else {
+    die("Erro SQL: " . $stmt->error);
+  }
+} else {
+  die("ERRO DE API!");
+}
