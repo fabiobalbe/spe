942ebde41c31aa3ce6a1e4a3441d1f8619e4b8ad
Fábio Balbé <fabiobalbe@mac.local>
2025-03-22 22:36:00 -0300

    tag </head> movida do index.php para o final do topo.php

--- componentes/topo.php
+++ componentes/topo.php
@@ -18,3 +18,4 @@ require_once dirname(__DIR__) . '/auth/access_control.php';
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js"></script>
+</head>
