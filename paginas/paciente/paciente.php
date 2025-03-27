3c2fd3c831dda49cf147f8b0750af0c7fb52401a
Fábio Balbé <fabiobalbe@mac.local>
2025-03-27 15:28:05 -0300

    Página do paciente populada

--- paginas/paciente/paciente.php
+++ paginas/paciente/paciente.php
@@ -28,19 +28,116 @@ if (!$row) {
 }
 
 ?>
-<!-- Título e botão cancelar -->
+<!-- Primeira linha: Título e botão voltar -->
 <div class="row">
   <div class="col-12 d-flex">
     <h2>Detalhes do paciente:</h2>
-
     <div class="d-flex ms-auto">
       <a href="/pacientes" class="btn btn-warning d-flex align-items-center">
         Voltar
       </a>
     </div>
-
   </div>
-
 </div>
 
-<span><?php echo htmlspecialchars($row['nome']); ?></span>
+<!-- Segunda Linha: Dados do paciente -->
+<div class="row mt-3">
+
+  <div class="col-6 d-flex">
+    <div class="card w-100 d-flex">
+      <div class="card-body">
+
+        <h4>
+          <?php echo htmlspecialchars($row['nome']); ?>
+        </h4>
+        </br>
+        <p>
+          <strong>
+            <small class="text-body-secondary">Idade: </small>
+          </strong>
+          <?php echo calcularIdade(htmlspecialchars($row['data_nascimento'])) . " anos."; ?>
+        </p>
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">Sexo: </small>
+          </strong>
+          <?php echo htmlspecialchars($row['sexo']); ?>
+        </p>
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">CPF: </small>
+          </strong>
+          <?php echo htmlspecialchars($row['cpf']); ?>
+        </p>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 d-flex">
+    <div class="card w-100 d-flex">
+      <div class="card-body">
+
+        <h4>
+          Informações de contato:
+        </h4>
+        </br>
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">Telefone: </small>
+          </strong>
+          <a href="http://wa.me/<?php echo htmlspecialchars($row['telefone']); ?>">
+            <?php echo htmlspecialchars($row['telefone']); ?>
+          </a>
+        </p>
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">e-mail: </small>
+          </strong>
+          <a href="mailto:<?php echo htmlspecialchars($row['email']); ?>">
+            <?php echo htmlspecialchars($row['email']); ?>
+          </a>
+        </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Terceira Linha: Dados médicos do paciente -->
+<div class="row mt-3">
+
+  <div class="col-12 d-flex">
+    <div class="card w-100 d-flex">
+      <div class="card-body">
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">Alergias: </small>
+          </strong>
+          <?php echo htmlspecialchars($row['alergias']); ?>
+        </p>
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">Observações: </small>
+          </strong>
+          <?php echo htmlspecialchars($row['observacoes']); ?>
+        </p>
+
+        <p>
+          <strong>
+            <small class="text-body-secondary">Histórico médico: </small>
+          </strong>
+          <?php echo htmlspecialchars($row['historico_medico']); ?>
+        </p>
+
+      </div>
+    </div>
+  </div>
+</div>
